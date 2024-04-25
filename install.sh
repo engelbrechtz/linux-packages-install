04a3c46c06038605dbc23daf008861c4e8a2269d
Declan Middleton <declanredd1722@gmail.com>
2024-04-25 16:08:21 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,8 @@
-sudo apt-get update 
-sudo apt-get install snapd -y
-sudo snap install code --classic
-sudo apt-get install git -y
-sudo apt-get install nmap -y
-sudo apt-get install john -y 
-sudo apt-get install whatweb -y
-sudo apt-get install net-tools -y
-sudo apt-get install netdiscover -y 
-sudo apt-get install python -y
-sudo apt-get install python3 -y
-sudo apt-get install pip3 -y
-sudo apt-get install sqlmap -y
-sudo apt-get install curl -y
-sudo apt-get install nodejs -y
-sudo apt-get install npm -y
-sudo apt-get install gobuster -y
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install git nmap john whatweb python python3 pip3 sqlmap curl nodejs npm gobuster -y
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && chmod 755 msfinstall && ./msfinstall
 git clone https://github.com/LionSec/katoolin.git && cp katoolin/katoolin.py /usr/bin/katoolin
 chmod +x /usr/bin/katoolin
 sudo apt-get update && sudo apt-get upgrade && echo "done installing..."
+
+
